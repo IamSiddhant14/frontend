6607fd6341ba96cb645d47909753891b495a36d0
Siddhant Sharma <siddhantsharma669@gmail.com>
2023-01-11 01:19:52 +0530

    Done with experince and skill section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
 import { urlFor , client } from '../../client';
-import { AppWrap } from '../../wrapper'
+import { AppWrap , MotionWrap } from '../../wrapper'
 
 const About = () => {
 
@@ -57,4 +57,9 @@ useEffect( () => {
   )
 }
 
-export default AppWrap(About , 'about');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
+
--- src/wrapper/index.js
+++ src/wrapper/index.js
@@ -1,1 +1,7 @@
-export{ default as AppWrap } from './AppWrap.js'
+import AppWrap from './AppWrap';
+import MotionWrap from './MotionWrap';
+
+export {
+  AppWrap,
+  MotionWrap,
+};
